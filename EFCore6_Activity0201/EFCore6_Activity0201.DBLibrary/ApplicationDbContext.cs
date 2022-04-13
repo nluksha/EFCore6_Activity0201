39e769236a9fa3acbf99d6313f4d164144ba4802
Nikita Luksha <nikita.luksha@softensity.com>
2022-04-13 20:39:44 +0300

    Init project

---
+++ EFCore6_Activity0201/EFCore6_Activity0201.DBLibrary/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore6_Activity0201.DBLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
