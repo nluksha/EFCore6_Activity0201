e1f9c31d8a3baed23101ad05c3951f4eee480ae0
Nikita Luksha <nikita.luksha@softensity.com>
2022-04-14 10:32:58 +0300

    Add app settings

--- EFCore6_Activity0201/EFCore6_Activity0201/Program.cs
+++ EFCore6_Activity0201/EFCore6_Activity0201/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot _configuration = BuildConfiguration();
+Console.WriteLine($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
+
+IConfigurationRoot BuildConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+    return builder.Build();
+}
